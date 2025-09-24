482069ab1e3a664a078a9de3f40c4d757d9206ca
hummusaki <gabriel05mortimer@gmail.com>
2025-09-24 11:57:54 -0700

    get media-ui ready

--- convert/js/media-ui.js
+++ convert/js/media-ui.js
@@ -1,18 +1,64 @@
-// import { convertFile } from "../js/media-convert.js";
-import { triggerDownload, toggleContrast } from '../js/utils.js';
+//import { convertFile } from './media-convert.js';
+import { triggerDownload, toggleContrast, setInitialContrast } from './utils.js';
 
 const fileInput = document.getElementById('fileInput');
 const statusMessage = document.getElementById('status');
 const fileFormat = document.getElementById('format');
 const convertBtn = document.getElementById('convertBtn');
-const downloadBtn = document.getElementById('downloadBtn');
+const downloadBtn = document.getElementById('bownloadBtn');
 const contrastBtn = document.getElementById('darkModeBtn');
 
-var contrastToggle = false;
+document.body.classList.add("no-transition");
 
+var contrastToggle = setInitialContrast();
 
+setTimeout(() => {
+    document.body.classList.remove("no-transition");
+}, 10);
 
+let convertedFileData = null;
+
+// convert button
+
+convertBtn.addEventListener('click', async() => {
+    const file = fileInput.files[0];  // get selected file
+    const outputFormat = fileFormat.value; // get current value
+
+    // check if file exists
+    if (!file) {
+        statusMessage.textContent = 'No media selected.';
+        return;
+    }
+
+    const inputType = file.type;  // get MIME type of the file
+    console.log('Selected media type:', inputType);
+
+    downloadBtn.disabled = true; // disable download button until conversion is done
+    statusMessage.textContent = 'Reading media...';
+    convertBtn.disabled = true; // prevent multiple uploads
+
+    
+    try{
+        
+    }
+    catch(error){
+        console.error('Conversion error:', error);
+        statusMessage.textContent = 'Conversion failed: ' + error.message;
+    }finally{
+        convertBtn.disabled = false;
+    }
+});
+
+// download button
+downloadBtn.addEventListener('click', () => {
+    if (convertedFileData) {
+        triggerDownload(convertedFileData.data, convertedFileData.fileName);
+    } else {
+        statusMessage.textContent = 'No converted file available for download.';
+    }
+});
 
 contrastBtn.addEventListener('click', () => {
     contrastToggle = toggleContrast(contrastToggle);
+    localStorage.setItem('contrastToggle', contrastToggle);
 });
